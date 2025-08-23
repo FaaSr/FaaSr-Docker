7a7c2ff576301e86a49b51c25c996abb46ebfb24
oneblueandred <nolanbailey21@gmail.com>
2025-08-23 16:36:25 -0700

    Update faasr_entry.py

--- faas_specific/faasr_entry.py
+++ faas_specific/faasr_entry.py
@@ -102,7 +102,7 @@ def get_payload_from_env(lambda_event=None):
     else:
         # store token in env for use in fetching file from gh
         token_present = store_pat_in_env(faasr_payload.overwritten["ComputeServers"])
-        logger.info("UseSecretStore off -- using overwritten")
+        logger.debug("UseSecretStore off - using overwritten")
 
     if not token_present:
         logger.info("Without a GitHub PAT in your workflow, you may hit rate limits")
@@ -147,7 +147,7 @@ def handler(event=None, context=None):
     log_sender = S3LogSender.get_log_sender()
     log_sender.flush_log()
 
-    faasr_msg = f"Finished action -- InvocationID: {faasr_payload['InvocationID']}"
+    faasr_msg = f"Finished action - InvocationID: {faasr_payload['InvocationID']}"
     logger.info(faasr_msg)
 
 
